d1d0a6fa5aacc3e2adfa215d44c0d0fec00d7d13
Andrei Lapets <a@lapets.io>
2021-11-01 14:58:31 -0400

    Add setup/build/installation requirements to package definition.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except ImportError:
     from distutils.command.build_clib import build_clib as _build_clib
 
 requirements = ["six", "cffi>=1.4.1"]
-setup_requirements = ["setuptools", "cffi>=1.4.1", "requests>=2.26.0"]
+install_requires = ["setuptools", "cffi>=1.4.1", "requests>=2.26.0"]
 
 def here(*paths):
     return os.path.relpath(os.path.join(*paths))
@@ -182,6 +182,7 @@ setup(
         "build_clib": build_clib,
         "build_ext": build_ext,
     },
+    install_requires=install_requires,
     distclass=Distribution,
     zip_safe=False
 )

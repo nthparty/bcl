ad45d58ee6423d2b8a646853b2f53728e9cd7f7e
Andrei Lapets <a@lapets.io>
2021-11-08 23:32:45 -0500

    Further streamline and restructure package build script.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ try:
 except ImportError:
     from distutils.command.build_clib import build_clib as _build_clib
 
-def prepare_libsodium_source_tree():
+def prepare_libsodium_source_tree(libsodium_folder='bcl/libsodium'):
     """
     Retrieve the libsodium source archive and extract it
     to the location used by the build process.
@@ -33,7 +33,6 @@ def prepare_libsodium_source_tree():
     )
     libsodium_tar_gz_path = './bcl/libsodium.tar.gz'
     libsodium_tar_gz_folder = './bcl/libsodium_tar_gz'
-    libsodium_folder = './bcl/libsodium'
 
     # Download the source archive to a local path (unless
     # it is already present).
@@ -66,49 +65,61 @@ def prepare_libsodium_source_tree():
     os.remove(libsodium_tar_gz_path)
     shutil.rmtree(libsodium_tar_gz_folder)
 
+    return libsodium_folder
+
 class Distribution(Distribution):
     def has_c_libraries(self):
-        # On Windows, only a precompiled dynamic library
-        # file is used.
-        return not sys.platform == "win32"
+        # On Windows, only a precompiled dynamic library file is used.
+        return not sys.platform == 'win32'
 
 class build_clib(_build_clib):
     def get_source_files(self):
-        files = glob.glob(os.path.relpath("bcl/libsodium/*"))
-        files += glob.glob(os.path.relpath("bcl/libsodium/*/*"))
-        files += glob.glob(os.path.relpath("bcl/libsodium/*/*/*"))
-        files += glob.glob(os.path.relpath("bcl/libsodium/*/*/*/*"))
-        files += glob.glob(os.path.relpath("bcl/libsodium/*/*/*/*/*"))
-        files += glob.glob(os.path.relpath("bcl/libsodium/*/*/*/*/*/*"))
-        files += glob.glob(os.path.relpath("bcl/libsodium/*/*/*/*/*/*/*"))
-        return files
+        return [
+            file
+            for i in range(1, 8)
+            for file in glob.glob(os.path.relpath('bcl/libsodium' + ('/*' * i)))
+        ]
 
     def build_libraries(self, libraries):
-        raise RuntimeError("`build_libraries` should not be invoked")
+        raise RuntimeError('`build_libraries` should not be invoked')
 
     def check_library_list(self, libraries):
-        raise RuntimeError("`check_library_list` should not be invoked")
+        raise RuntimeError('`check_library_list` should not be invoked')
 
     def get_library_names(self):
-        return ["sodium"]
+        return ['sodium']
 
     def run(self):
-        # On Windows, only a precompiled dynamic library
-        # file is used.
-        if sys.platform == "win32":
+        # On Windows, only a precompiled dynamic library file is used.
+        if sys.platform == 'win32':
             return
 
-        # Retrieve (if necessary) and extract the libsodium source tree.
-        prepare_libsodium_source_tree()
+        # Confirm that make utility can be found.
+        found = False
+        if not os.environ.get('PATH', None) is None:
+            for p in os.environ.get('PATH', '').split(os.pathsep):
+                p = os.path.join(p, 'make')
+                if os.access(p, os.X_OK):
+                    found = True
+                for e in filter(
+                    None,
+                    os.environ.get('PATHEXT', '').split(os.pathsep)
+                ):
+                    if os.access(p + e, os.X_OK):
+                        found = True
+        if not found:
+            raise RuntimeError('make utility cannot be found')
 
-        # Use Python's build environment variables.
-        build_env = {
-            key: val
-            for key, val in get_config_vars().items()
-            if key in ("LDFLAGS", "CFLAGS", "CC", "CCSHARED", "LDSHARED")
-            and key not in os.environ
-        }
-        os.environ.update(build_env)
+        # Reproduce Python's build environment variables.
+        os.environ.update({
+            variable: value
+            for (variable, value) in get_config_vars().items()
+            if (
+                variable in [
+                    'LDFLAGS', 'CFLAGS', 'CC', 'CCSHARED', 'LDSHARED'
+                ] and variable not in os.environ
+            )
+        })
 
         # Ensure the temporary build directory exists.
         build_temp = os.path.abspath(self.build_temp)
@@ -118,61 +129,40 @@ class build_clib(_build_clib):
             if e.errno != errno.EEXIST:
                 raise
 
+        # Retrieve (if necessary) and extract the libsodium source tree.
+        libsodium_folder = prepare_libsodium_source_tree()
+
         # Ensure that all executable files have the necessary permissions.
-        prefix = 'bcl/libsodium/'
         for filename in [
-            "autogen.sh", "compile", "configure", "depcomp", "install-sh",
-            "missing", "msvc-scripts/process.bat", "test/default/wintest.bat",
+            'autogen.sh', 'compile', 'configure', 'depcomp', 'install-sh',
+            'missing', 'msvc-scripts/process.bat', 'test/default/wintest.bat',
         ]:
-            os.chmod(os.path.relpath(prefix + filename), 0o755)
+            os.chmod(os.path.relpath(libsodium_folder + '/' + filename), 0o755)
 
-        # Confirm that make utility can be found.
-        found = False
-        if not os.environ.get("PATH", None) is None:
-            for p in os.environ.get("PATH", "").split(os.pathsep):
-                p = os.path.join(p, "make")
-                if os.access(p, os.X_OK):
-                    found = True
-                for e in filter(
-                    None,
-                    os.environ.get("PATHEXT", "").split(os.pathsep)
-                ):
-                    if os.access(p + e, os.X_OK):
-                        found = True
-        if not found:
-            raise RuntimeError("make utility cannot be found")
-
-        # Determine location of libsodium configuration script and
-        # configure libsodium.
+        # Configure libsodium, build it as a shared library file, check it,
+        # and install it.
         subprocess.check_call(
-            [os.path.abspath(os.path.relpath("bcl/libsodium/configure"))] + \
+            [os.path.abspath(os.path.relpath('bcl/libsodium/configure'))] + \
             [
-                "--disable-shared", "--enable-static",
-                "--disable-debug", "--disable-dependency-tracking", "--with-pic",
+                '--disable-shared', '--enable-static',
+                '--disable-debug', '--disable-dependency-tracking', '--with-pic',
             ] + \
-            (["--disable-ssp"] if platform.system() == "SunOS" else []) + \
-            ["--prefix", os.path.abspath(self.build_clib)],
+            (['--disable-ssp'] if platform.system() == 'SunOS' else []) + \
+            ['--prefix', os.path.abspath(self.build_clib)],
             cwd=build_temp
         )
-
-        make_args = os.environ.get("LIBSODIUM_MAKE_ARGS", "").split()
-
-        # Build the library.
-        subprocess.check_call(["make"] + make_args, cwd=build_temp)
-
-        # Check the built library.
-        subprocess.check_call(["make", "check"] + make_args, cwd=build_temp)
-
-        # Install the built library.
-        subprocess.check_call(["make", "install"] + make_args, cwd=build_temp)
+        make_args = os.environ.get('LIBSODIUM_MAKE_ARGS', '').split()
+        subprocess.check_call(['make'] + make_args, cwd=build_temp)
+        subprocess.check_call(['make', 'check'] + make_args, cwd=build_temp)
+        subprocess.check_call(['make', 'install'] + make_args, cwd=build_temp)
 
 class build_ext(_build_ext):
     def run(self):
         if self.distribution.has_c_libraries():
-            build_clib = self.get_finalized_command("build_clib")
-            self.include_dirs.append(os.path.join(build_clib.build_clib, "include"),)
-            self.library_dirs.insert(0, os.path.join(build_clib.build_clib, "lib64"),)
-            self.library_dirs.insert(0, os.path.join(build_clib.build_clib, "lib"),)
+            build_clib = self.get_finalized_command('build_clib')
+            self.include_dirs.append(os.path.join(build_clib.build_clib, 'include'),)
+            self.library_dirs.insert(0, os.path.join(build_clib.build_clib, 'lib64'),)
+            self.library_dirs.insert(0, os.path.join(build_clib.build_clib, 'lib'),)
 
         return _build_ext.run(self)
 

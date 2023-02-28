6a72140ab001bddaa35d0e1a8837c699f2bf9062
Wyatt Howe <whowe@magnite.com>
2023-02-28 15:51:38 -0500

    Try to fix a path error in `setup.py`

--- setup.py
+++ setup.py
@@ -132,7 +132,8 @@ def render_sodium():
             path_to_sodium, "rb"
         ).read().hex()
     }
-    template = open(f"{extract_current_build_path()}/_sodium.tmpl", encoding='utf-8').read()  # pylint: disable=consider-using-with
+    print('\n\n\n debug \n\n\n', os.listdir(extract_current_build_path()), '?\n\n\n')
+    template = open(os.path.join(extract_current_build_path(), _sodium.tmpl), encoding='utf-8').read()  # pylint: disable=consider-using-with
 
     # Emit rendered file to build directory
     with open(f"{extract_current_build_path()}/_sodium.py", "w", encoding='utf-8') as sodium_out:
